bfc05e97034ccb9f09f4db1286d256d4b5e4bf5f
Jannik Lorenz <dev@janniklorenz.de>
2020-05-17 01:44:25 +0200

    deactivate dsc api

--- src/app/dsc-api.service.ts
+++ src/app/dsc-api.service.ts
@@ -47,49 +47,49 @@ export class DscApiService {
       this.auth.key = params.get("key");
     })
     
-    this.socket = io("http://10.1.0.211:3000");
+    // this.socket = io("http://10.1.0.211:3000");
     // this.socket = io(environment.serverURL);
     // this.socket = io("ws://" + location.host + "/socket/");
     // this.socket = io("ws://" + location.host);
     
-    this.socket.on('connect', () => {
-      console.log('on connect');
-      this._connected.next(true);
-    });
-    
-    this.socket.on('disconnect', () => {
-      console.log('on disconnect');
-      this._connected.next(false);
-    });
-    
-    this.socket.on('setStatus', (connected) => {
-      console.log('setStatus', connected);
-    });
-    
-    
-    
-    this.socket.on('showMessage', (message) => {
-      console.log('showMessage', message);
-    });
-    this.socket.on('hideMessage', () => {
-      console.log('hideMessage');
-    });
-    
-    
+    // this.socket.on('connect', () => {
+    //   console.log('on connect');
+    //   this._connected.next(true);
+    // });
+    // 
+    // this.socket.on('disconnect', () => {
+    //   console.log('on disconnect');
+    //   this._connected.next(false);
+    // });
+    // 
+    // this.socket.on('setStatus', (connected) => {
+    //   console.log('setStatus', connected);
+    // });
+    // 
+    // 
+    // 
+    // this.socket.on('showMessage', (message) => {
+    //   console.log('showMessage', message);
+    // });
+    // this.socket.on('hideMessage', () => {
+    //   console.log('hideMessage');
+    // });
     
     
     
     
-    this.socket.on('setData', (session) => {
-      this.currentSession = session;
-      this._session.next(session);
-      console.log('setData', session);
-    });
     
-    this.socket.on('setConfig', (config) => {
-      console.log('setConfig', config);
-      this._config.next(config);
-    });
+    
+    // this.socket.on('setData', (session) => {
+    //   this.currentSession = session;
+    //   this._session.next(session);
+    //   console.log('setData', session);
+    // });
+    // 
+    // this.socket.on('setConfig', (config) => {
+    //   console.log('setConfig', config);
+    //   this._config.next(config);
+    // });
     
     
     
@@ -155,34 +155,34 @@ export class DscApiService {
   
 
   setNewTarget() {
-    this.socket.emit("newTarget", {
-      auth: this.auth,
-    });
+    // this.socket.emit("newTarget", {
+    //   auth: this.auth,
+    // });
   }
   setPart(partId, forceNewPart) {
-    console.log(partId);
-    this.socket.emit("setPart", {
-      auth: this.auth,
-      partId: partId,
-    });
+    // console.log(partId);
+    // this.socket.emit("setPart", {
+    //   auth: this.auth,
+    //   partId: partId,
+    // });
   }
   togglePart() {
-    const session = this.currentSession;
-    if (session == null) return;
-    const partsOrder = session.disziplin.partsOrder;
-    const activePart = session.sessionParts[session.sessionIndex];
-    const currentIndex = partsOrder.indexOf(activePart.type)
-    
-    console.log(currentIndex)
-    
-    // Jump to the first disciplin part if we are at the end
-    if (currentIndex + 1 >= partsOrder.length) {
-      this.setPart(partsOrder[0], false);
-    }
-    // Jump to the nex disciplin part
-    else {
-      this.setPart(partsOrder[currentIndex+1], false);
-    }
+    // const session = this.currentSession;
+    // if (session == null) return;
+    // const partsOrder = session.disziplin.partsOrder;
+    // const activePart = session.sessionParts[session.sessionIndex];
+    // const currentIndex = partsOrder.indexOf(activePart.type)
+    // 
+    // console.log(currentIndex)
+    // 
+    // // Jump to the first disciplin part if we are at the end
+    // if (currentIndex + 1 >= partsOrder.length) {
+    //   this.setPart(partsOrder[0], false);
+    // }
+    // // Jump to the nex disciplin part
+    // else {
+    //   this.setPart(partsOrder[currentIndex+1], false);
+    // }
   }
   setSessionIndex(sessionIndex) {
     // this.socket.emit("setSessionIndex", {
@@ -209,19 +209,19 @@ export class DscApiService {
     // });
   }
   setDisciplin(disziplin) {
-    this.socket.emit("setDisziplin", {
-      auth: this.auth,
-      disziplin: disziplin,
-    });
+    // this.socket.emit("setDisziplin", {
+    //   auth: this.auth,
+    //   disziplin: disziplin,
+    // });
   }
   print() {
     // default Normal
     // dateless Ohne Datum
     // bigImage Größere Scheibe
-    this.socket.emit("print", {
-      auth: this.auth,
-      printTemplate: "default",
-    });
+    // this.socket.emit("print", {
+    //   auth: this.auth,
+    //   printTemplate: "default",
+    // });
   }
   loadData(data) {
     // this.socket.emit("loadData", {
